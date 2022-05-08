ef45940e4d181ed445b1f201d4037c44de622ff1
yash-2507 <99003521+yash-2507@users.noreply.github.com>
2022-05-08 19:35:30 +0530

    CheckOut Page Added

--- src/App.js
+++ src/App.js
@@ -6,19 +6,21 @@ import SignUpModal from "./components/SignUpModal";
 import Index from "./Pages/Index";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import CheckOut from "./Pages/CheckOut";
 
 function App() {
-  const { isOpen } = useSelector((store) => store.modal);
-  const { isLoginOpen, isSignUpOpen } = useSelector((store) => store.login);
-  return (
-    <div className="App">
-      {/* <Navbar /> */}
-      {isLoginOpen && <LoginModal />}
-      {isSignUpOpen && <SignUpModal />}
-      {isOpen && <ModalMain />}
-      <Index />
-    </div>
-  );
+    const { isOpen } = useSelector((store) => store.modal);
+    const { isLoginOpen, isSignUpOpen } = useSelector((store) => store.login);
+    return (
+        <div className="App">
+            {/* <Navbar /> */}
+            {isLoginOpen && <LoginModal />}
+            {isSignUpOpen && <SignUpModal />}
+            {isOpen && <ModalMain />}
+            {/* <Index /> */}
+            <CheckOut />
+        </div>
+    );
 }
 
 export default App;
